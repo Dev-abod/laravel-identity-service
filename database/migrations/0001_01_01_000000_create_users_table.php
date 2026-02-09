5567a614d2b9f3caab17e605f9bde23650ab0491
AlaaAl-deen <alaaalselwi2020@gmail.com>
2026-02-09 17:17:19 +0300

    Add database schema with  migrations

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -12,13 +12,14 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+    $table->id();
+    $table->string('university_id')->unique();
+    $table->string('email')->nullable()->unique();
+    $table->timestamp('email_verified_at')->nullable();
+    $table->string('password');
+    $table->boolean('must_change_password')->default(true);
+    $table->rememberToken();
+    $table->timestamps();
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
