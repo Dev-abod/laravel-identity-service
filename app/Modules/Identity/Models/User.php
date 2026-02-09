3244717ca8b9f3c3da65002497e7eb391d9fa9b0
AlaaAl-deen <alaaalselwi2020@gmail.com>
2026-02-09 17:23:50 +0300

    Add and Transfer model users

---
+++ app/Modules/Identity/Models/User.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Modules\Identity\Models;
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+class User extends Authenticatable implements MustVerifyEmail
+{
+    use HasApiTokens, Notifiable;
+
+    protected $fillable = [
+        'university_id',
+        'email',
+        'password',
+        'must_change_password',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'must_change_password' => 'boolean',
+        ];
+    }
+}
